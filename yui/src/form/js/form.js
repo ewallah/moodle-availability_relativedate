4143c6f3b2d7b79961471e949fbe96931bfafd39
eWallah <info@eWallah.net>
2019-03-02 18:32:13 +0100

    grunt lint errors fixed

--- yui/src/form/js/form.js
+++ yui/src/form/js/form.js
@@ -24,21 +24,22 @@ M.availability_relativedate.form.initInner = function(timeFields, startFields) {
 
 M.availability_relativedate.form.getNode = function(json) {
     // Create HTML structure.
-    var strings = M.str.availability_relativedate;
     var html = '<span class="availability-relativedate"><label><select name="relativenumber">';
-    for (var i = 1; i < 52; i++) {
+    var i = 0;
+    var fieldInfo;
+    for (i = 1; i < 52; i++) {
         html += '<option value="' + i + '">' + i + '</option>';
     }
 
     html += '</select></label> ';
     html += '<label><select name="relativednw">';
-    for (var i = 0; i < this.timeFields.length; i++) {
+    for (i = 0; i < this.timeFields.length; i++) {
         fieldInfo = this.timeFields[i];
         html += '<option value="' + fieldInfo.field + '">' + fieldInfo.display + '</option>';
     }
     html += '</select></label> ';
     html += '<label><select name="relativestart">';
-    for (var i = 0; i < this.startFields.length; i++) {
+    for (i = 0; i < this.startFields.length; i++) {
         fieldInfo = this.startFields[i];
         html += '<option value="' + fieldInfo.field + '">' + fieldInfo.display + '</option>';
     }
@@ -79,9 +80,9 @@ M.availability_relativedate.form.getNode = function(json) {
 };
 
 M.availability_relativedate.form.fillValue = function(value, node) {
-    value.n = parseInt(node.one('select[name=relativenumber]').get('value'));
-    value.d = parseInt(node.one('select[name=relativednw]').get('value'));
-    value.s = parseInt(node.one('select[name=relativestart]').get('value'));
+    value.n = node.one('select[name=relativenumber]').get('value');
+    value.d = node.one('select[name=relativednw]').get('value');
+    value.s = node.one('select[name=relativestart]').get('value');
 };
 
 M.availability_relativedate.form.fillErrors = function(errors, node) {
