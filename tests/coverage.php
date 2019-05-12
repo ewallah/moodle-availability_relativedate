4549197df2428fbbc48ea01ba4404c654cfc05e1
eWallah <info@eWallah.net>
2019-05-12 19:56:48 +0200

    version update

--- tests/coverage.php
+++ tests/coverage.php
@@ -32,7 +32,7 @@ return new class extends phpunit_coverage_info {
     protected $whitelistfiles = [];
 
     /** @var array The list of folders relative to the plugin root to excludelist in coverage generation. */
-    protected $excludelistfolders = [];
+    protected $excludelistfolders = ['tests'];
 
     /** @var array The list of files relative to the plugin root to excludelist in coverage generation. */
     protected $excludelistfiles = [];
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019032101;
+$plugin->version = 2019051200;
 $plugin->requires = 2018051700;
 $plugin->component = 'availability_relativedate';
 $plugin->release = '3.5+';
