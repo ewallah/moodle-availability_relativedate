1d969afe715ee1972508379ce5ac769dcf04dad1
Renaat Debleu <rdebleu@eWallah.net>
2022-01-06 15:06:12 +0000

    Version update

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021121200;
 $plugin->requires  = 2020061500;
 $plugin->component = 'availability_relativedate';
-$plugin->release   = '3.9+';
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->release = 'v3.9.6';
+$plugin->version = 2022010600;
